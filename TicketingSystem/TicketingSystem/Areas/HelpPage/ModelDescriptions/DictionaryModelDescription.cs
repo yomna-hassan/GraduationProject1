0bb560e1a0230ead948c8e946e13f21704818eb2
yomna-hassan <36695085+yomna-hassan@users.noreply.github.com>
2018-05-23 17:46:46 +0200

    first

---
+++ TicketingSystem/TicketingSystem/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TicketingSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TicketingSystem/TicketingSystem/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TicketingSystem.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
